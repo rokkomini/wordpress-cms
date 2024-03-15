006d959f8224455738053f970b455ea47cf36c99
mika <mikahagerstrom@hotmail.com>
2024-03-15 11:51:25 +0100

    slide effect added to nav overlay

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -3,14 +3,16 @@ import { FiX } from "react-icons/fi";
 
 export default function SideMenu({ showMenu, exitMenu }: any) {
   return (
-    <aside className='flex side-menu' style={showMenu ? { display: 'block' } : { display: 'none' }}>
-      <FiX onClick={exitMenu}/>
-      <div className='overlay' />
-      <div className='flex header-links content'>
-        <a href="">Kontakt</a>
-        <a href="">Om oss</a>
-        <a href="">Hagsätra</a>
-        <a href="">Stockholm Centralstation</a>
+    <aside className={showMenu ? 'side-menu show' : 'side-menu'}>
+      <div className="flex start">
+        <FiX onClick={exitMenu}/>
+        <div className='overlay' />
+        <div className='flex start header-links content'>
+          <a href="">Kontakt</a>
+          <a href="">Om oss</a>
+          <a href="">Hagsätra</a>
+          <a href="">Stockholm Centralstation</a>
+        </div>
       </div>
     </aside>
   )
